b4806efeac676c60187c3a77d7930f0ed1d57a20
AdhirajNakhe <nakhe93@ufl.edu>
2017-10-17 11:52:25 -0400

    Added test for login component loading

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {AuthService} from '../services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import {FlashMessagesModule} from 'angular2-flash-messages';
 import { LoginComponent } from './login.component';
+import {  MatButtonModule, } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +14,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [
+        MatButtonModule,
+        FlexLayoutModule,
+        HttpModule,
+        FormsModule,
+        FlashMessagesModule,
+        RouterTestingModule.withRoutes([{ path: 'home', component: LoginComponent }])
+      ],
+      declarations: [ LoginComponent ],
+      providers: [AuthService]
     })
     .compileComponents();
   }));
