a7fe5c8f27c723bbc5abf77f47ab5828e4317ba7
gdeepchadha <gdeepchadha@ufl.edu>
2017-11-06 14:18:54 -0500

    - Changed database from mongodb to elasticsearch and made changes to restaurants and user routers, also changing helper file passport.js - Added mapping.js to create mapping into elasticsearch db on local system - Added addData.js to addData into elasticsearch db on local system - Deleted models and insertRestaurant router

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,12 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/user');
 const config = require('./database');
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
 
 module.exports = function(passport){
   let opts = {};
@@ -10,11 +15,10 @@ module.exports = function(passport){
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     console.log("jwt_payload => "+JSON.stringify(jwt_payload));
     console.log("id: "+jwt_payload.data._id);
-    User.getUserById(jwt_payload.data._id, (err, user) => {
+    getUserById(jwt_payload.data._id, (err, user) => {
       if(err){
         return done(err, false);
       }
- 
       if(user){
         return done(null, user);
       } else { 
@@ -24,3 +28,21 @@ module.exports = function(passport){
   }));
 } 
 
+function getUserbyId(id, callback) {
+  var query = {
+    "query": {
+      "terms": {
+        "_id": [id]
+      }
+    }
+  };
+
+  client.search({
+    index: 'gulp',
+    type: 'restaurants',
+    body: query
+  }, function (error, response) {
+
+    callback(error, response.hits.hits[0]._source)
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -3,106 +3,88 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
-const User = require('../models/user');
-var mongoose = require('mongoose');
+
+var elasticsearch = require('elasticsearch');
+const client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
 
 router.post('/register', (req, res, next) => {
-  mongoose.connect(config.database, 
-    { server: {
-        reconnectTries: Number.MAX_VALUE,
-        reconnectInterval: 1000 
-        } 
-    }
-  );
 
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
+    console.log("register user");
+    if(!(req.body.name && req.body.email && req.body.username && req.body.password))
+        return res.send('400', { error: 'User details expected. Expected : name, email, username, password' });
+    
+    if(typeof req.body.name != "string" || typeof req.body.email!="string" || typeof req.body.username!="string" || typeof req.body.password!="string")
+        return res.send('400', { error: 'User details expected. Expected : name, email, username, password' });
+    
+    var newUser = {
+        "name": req.body.name,
+        "email": req.body.email,
+        "username": req.body.username,
+        "password": req.body.password
+    };
 
-  db.once('open', function () {
-    console.log("***Connected correctly to server");
-    let newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password
-    });
-
-    /**
-    * Method is exported from model/user.js
-    */
-    User.addUser(newUser, (err, user) => {
-      console.log('**** IN ADDUSER() *****');
-      if(err){
-        console.log('**** IN ADDUSER().err *****');
-        res.json({success: false, msg:'Failed to register user'}); 
-      } else {
-        console.log('**** IN ADDUSER().success *****');
-        res.json({success: true, msg:'User registered'});
-      }
-      console.log('**** CLOSING DB***');
-      mongoose.disconnect();
+    addUser(newUser, (err, user) => {
+        console.log('**** IN ADDUSER() *****');
+        if (err) {
+            console.log('**** IN ADDUSER().err *****');
+            console.log(err);
+            res.json({ success: false, msg: 'Failed to register user' });
+        } else {
+            console.log('**** IN ADDUSER().success *****');
+            res.json({ success: true, msg: 'User registered' });
+        }
     });   
-  });
 });
 
 /**
  * Method is exported from model/user.js
 */
 router.post('/authenticate', (req, res, next) => {
-  mongoose.connect(config.database, 
-    { server: {
-        reconnectTries: Number.MAX_VALUE,
-        reconnectInterval: 1000 
-        } 
-    }
-  );
+  console.log('**** IN Authenticate***');
+  if (!(req.body.username && req.body.password))
+      return res.send('400', { error: 'User details expected. Expected : username, password' });
+  if (typeof req.body.username != "string" || typeof req.body.password != "string")
+      return res.send('400', { error: 'User details expected. Expected : username, password of type string' });
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log('**** username = ' + username);
+  console.log('**** password = ' + password);
 
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-
-  db.once('open', function () {
-    console.log("Connected correctly to server");
-    console.log('**** IN Authenticate***');
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log('**** username = '+username);
-    console.log('**** password = '+password);
-
-    User.getUserByUsername(username, (err, user) => {
+  getUserByUsername(username, (err, user) => {
       console.log('*** getUserByUsername');
-      if(err) 
-        throw err;
-      if(!user){
-         res.json({success: false, msg: 'User not found'});
-         console.log('*** diconnecting... ');
-         mongoose.disconnect();
-         return;
+      if (err)
+          throw err;
+      if (!user) {
+          res.json({ success: false, msg: 'User not found' });
+          return;
       }
 
-      User.comparePassword(password, user.password, (err, isMatch) => {
-        console.log('*** comparePassword');
-        if (err) throw err;
-        if (isMatch) {
-          const token = jwt.sign({data: user}, config.secret); 
+      comparePassword(password, user._source.password, (err, isMatch) => {
+          console.log('*** comparePassword');
+          if (err) {
+              console.log("Error found in compare password - bcrypt");
+          }
+          if (isMatch) {
+              const token = jwt.sign({ data: user }, config.secret);
 
-          res.json({
-            success: true,
-            token: 'JWT '+token,
-            user: {
-              id: user._id,
-              name: user.name,
-              username: user.username,
-              email: user.email
-            }
-          });
-        } else {
-           res.json({success: false, msg: 'Wrong password'}); 
-        }
-        
-        console.log('*** diconnecting... ');
-        mongoose.disconnect();
-      });    
-    });
+              console.log("User recognised");
+              res.json({
+                  success: true,
+                  token: 'JWT ' + token,
+                  user: {
+                      id: user._id,
+                      name: user._source.name,
+                      username: user._source.username,
+                      email: user._source.email
+                  }
+              });
+          } else {
+              res.json({ success: false, msg: 'Wrong password' });
+          }
+      });
   });
 });
 
@@ -110,5 +92,79 @@ router.post('/authenticate', (req, res, next) => {
 router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => { 
   res.json({user: req.user});
 });
+
+//helper functions
+const bcrypt = require('bcryptjs');
+
+function getUserByUsername(username, callback) {
+    var query = {
+        "query": {
+            "match": {
+                "username": username
+            }
+        }
+    };
+    client.search({
+        index: 'gulp',
+        type: 'users',
+        body: query
+    }, function(error, response){
+      callback(error,response.hits.hits[0]);
+  });
+}
+
+function addUser(newUser, callback) {
+    var query = {
+        "query": {
+            "match": {
+                "username": newUser.username
+            }
+        }
+    };
+    client.search({
+        index: 'gulp',
+        type: 'users',
+        body: query
+    }, (error, response) => {
+        console.log('----- res = ' + JSON.stringify(response));
+        if(error)
+          throw error;
+        var res=response.hits.hits;
+        console.log("");
+        console.log("???????????");
+        console.log("***************"+JSON.stringify(res));
+        if (res[0]!=null || res[0]!=undefined) {
+            callback(new Error("User already exists!"));
+        }
+        else {
+            console.log('****In add User***');
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    console.log('****saving user***');
+                    client.index({
+                        index: 'gulp',
+                        type: 'users',
+                        body: newUser
+                      }, callback);
+                });
+            });
+        }
+
+    });
+}
+
+function comparePassword(candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if (err) {
+            console.log("-----------------------------");
+            console.log(err);
+            console.log("-----------------------------");
+            callback(err, false);
+        }
+        callback(null, isMatch);
+    });
+}
 
 module.exports = router;
