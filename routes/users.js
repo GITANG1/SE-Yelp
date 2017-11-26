c1a3bc6ede32ba9b172e882e888221838d884e1f
AdhirajNakhe <nakhe93@ufl.edu>
2017-11-26 14:32:34 -0500

    Added functionality to support user profile updates

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,43 @@ const config = require('../config/database');
 
 var elasticsearch = require('elasticsearch');
 const client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'trace'
+    host: 'localhost:9200',
+    log: 'trace'
 });
+router.put('/update', (req, res, next) => {
+    if (!(req.body.id && req.body.name && req.body.email))
+        return res.json({ msg: 'Failed to update user' });
 
+    console.log(req.body.name);
+    var hashedPassword;
+    if (req.body.password) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(req.body.password, salt, (err, hash) => {
+                if (err) throw err;
+                hashedPassword = hash;
+                console.log('****saving user***');
+                var script = "ctx._source.name=\"" + String(req.body.name) + "\"; ctx._source.email=\"" + String(req.body.email) + "\"; ctx._source.password=\"" + String(hashedPassword) + "\";";
+                updateUser(req.body.id, script, res);
+            });
+
+        });
+    }
+
+    else {
+        var script = "ctx._source.name=\"" + String(req.body.name) + "\"; ctx._source.email=\"" + String(req.body.email) + "\";";
+        updateUser(req.body.id, script, res);
+    }
+
+});
 router.post('/register', (req, res, next) => {
     console.log(config.DB);
     console.log("register user");
-    if(!(req.body.name && req.body.email && req.body.username && req.body.password))
-        return res.json({ msg:'Failed to register user'});
-    
-    if(typeof req.body.name != "string" || typeof req.body.email!="string" || typeof req.body.username!="string" || typeof req.body.password!="string")
-        return res.json({ msg:'Failed to register user'});
-    
+    if (!(req.body.name && req.body.email && req.body.username && req.body.password))
+        return res.json({ msg: 'Failed to register user' });
+
+    if (typeof req.body.name != "string" || typeof req.body.email != "string" || typeof req.body.username != "string" || typeof req.body.password != "string")
+        return res.json({ msg: 'Failed to register user' });
+
     var newUser = {
         "name": req.body.name,
         "email": req.body.email,
@@ -36,61 +60,61 @@ router.post('/register', (req, res, next) => {
             console.log('**** IN ADDUSER().success *****');
             res.json({ success: true, msg: 'User registered' });
         }
-    });   
+    });
 });
 
 /**
  * Method is exported from model/user.js
 */
 router.post('/authenticate', (req, res, next) => {
-  console.log('**** IN Authenticate***');
-  if (!(req.body.username && req.body.password))
-      return res.send('400', { error: 'User details expected. Expected : username, password' });
-  if (typeof req.body.username != "string" || typeof req.body.password != "string")
-      return res.send('400', { error: 'User details expected. Expected : username, password of type string' });
-  var username = req.body.username;
-  var password = req.body.password;
-  console.log('**** username = ' + username);
-  console.log('**** password = ' + password);
+    console.log('**** IN Authenticate***');
+    if (!(req.body.username && req.body.password))
+        return res.send('400', { error: 'User details expected. Expected : username, password' });
+    if (typeof req.body.username != "string" || typeof req.body.password != "string")
+        return res.send('400', { error: 'User details expected. Expected : username, password of type string' });
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log('**** username = ' + username);
+    console.log('**** password = ' + password);
 
-  getUserByUsername(username, (err, user) => {
-      console.log('*** getUserByUsername');
-      if (err)
-          throw err;
-      if (!user) {
-          res.json({ success: false, msg: 'User not found' });
-          return;
-      }
+    getUserByUsername(username, (err, user) => {
+        console.log('*** getUserByUsername');
+        if (err)
+            throw err;
+        if (!user) {
+            res.json({ success: false, msg: 'User not found' });
+            return;
+        }
 
-      comparePassword(password, user._source.password, (err, isMatch) => {
-          console.log('*** comparePassword');
-          if (err) {
-              console.log("Error found in compare password - bcrypt");
-          }
-          if (isMatch) {
-              const token = jwt.sign({ data: user }, config.secret);
+        comparePassword(password, user._source.password, (err, isMatch) => {
+            console.log('*** comparePassword');
+            if (err) {
+                console.log("Error found in compare password - bcrypt");
+            }
+            if (isMatch) {
+                const token = jwt.sign({ data: user }, config.secret);
 
-              console.log("User recognised");
-              res.json({
-                  success: true,
-                  token: 'JWT ' + token,
-                  user: {
-                      id: user._id,
-                      name: user._source.name,
-                      username: user._source.username,
-                      email: user._source.email
-                  }
-              });
-          } else {
-              res.json({ success: false, msg: 'Wrong password' });
-          }
-      });
-  });
+                console.log("User recognised");
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token,
+                    user: {
+                        id: user._id,
+                        name: user._source.name,
+                        username: user._source.username,
+                        email: user._source.email
+                    }
+                });
+            } else {
+                res.json({ success: false, msg: 'Wrong password' });
+            }
+        });
+    });
 });
 
 // Profile
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => { 
-  res.json({user: req.user});
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.json({ user: req.user });
 });
 
 //helper functions
@@ -104,14 +128,32 @@ function getUserByUsername(username, callback) {
             }
         }
     };
-    
+
     client.search({
         index: config.DB,
         type: 'users',
         body: query
-    }, function(error, response){
-      callback(error,response.hits.hits[0]);
-  });
+    }, function (error, response) {
+        callback(error, response.hits.hits[0]);
+    });
+}
+
+function getUserById(id, callback) {
+    var query = {
+        "query": {
+            "match": {
+                "_id": id
+            }
+        }
+    };
+
+    client.search({
+        index: config.DB,
+        type: 'users',
+        body: query
+    }, function (error, response) {
+        callback(error, response.hits.hits[0]);
+    });
 }
 
 function addUser(newUser, callback) {
@@ -128,18 +170,18 @@ function addUser(newUser, callback) {
         body: query
     }, (error, response) => {
         console.log('----- res = ' + JSON.stringify(response));
-        if(error)
-          throw error;
-        var res=response.hits.hits;
+        if (error)
+            throw error;
+        var res = response.hits.hits;
         console.log("");
         console.log("???????????");
-        console.log("***************"+JSON.stringify(res));
-        if (res[0]!=null || res[0]!=undefined) {
+        console.log("***************" + JSON.stringify(res));
+        if (res[0] != null || res[0] != undefined) {
             callback(new Error("User already exists!"));
         }
         else {
             console.log('****In add User***');
-           
+
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
@@ -149,7 +191,7 @@ function addUser(newUser, callback) {
                         index: config.DB,
                         type: 'users',
                         body: newUser
-                      }, callback);
+                    }, callback);
                 });
             });
         }
@@ -166,6 +208,40 @@ function comparePassword(candidatePassword, hash, callback) {
             callback(err, false);
         }
         callback(null, isMatch);
+    });
+}
+
+function updateUser(id, script, res) {
+    getUserById(id, (err, user) => {
+        console.log('*** getUserById');
+        if (err) {
+            res.status(500)
+                .send("Error");
+            return;
+        }
+        if (!user) {
+            res.json({ success: false, msg: 'User not found' });
+            return;
+        }
+
+        client.update({
+            index: config.DB,
+            type: 'users',
+            id: id,
+            body: {
+                script: script,
+            }
+        }, function (error, response) {
+            if (error) {
+                console.log(error);
+                res.status(500)
+                    .send("Error inserting the profile changes in the database");
+                return;
+            }
+            res.status(201)
+                .send("user updates");
+            return;
+        });
     });
 }
 
